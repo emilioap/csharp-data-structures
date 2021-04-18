7b1e5673207c7bd0c08d49b280891455c87336e3
Emilio Pagnoca <emiliopagnoca@gmail.com>
2021-04-18 16:52:05 -0300

    Little changes

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -9,10 +9,10 @@ namespace DataStructures
             int result;
             Console.WriteLine("Choose your data structure by typing the corresponding number: \n");
             Console.WriteLine("1 - Stack");
-            Console.WriteLine("2- Queue");
-            bool validOption = int.TryParse(Console.ReadLine(), out result);
+            Console.WriteLine("2 - Queue");
+            bool isValidOption = int.TryParse(Console.ReadLine(), out result);
 
-            if (validOption)
+            if (isValidOption)
             {
                 var exec = new StrategyRouter(result);
                 exec.ExecuteStructureType();
